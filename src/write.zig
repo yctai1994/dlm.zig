014f1e8150863f934ff08fdc3830519936981127
yctai1994 <yctai.git@gmail.com>
2024-03-11 16:30:51 +0900

    edit: `write`

--- src/write.zig
+++ src/write.zig
@@ -8,7 +8,25 @@ fn hexFromInt(int: u64) u8 {
     };
 }
 
-pub fn write(buffer: []u8, val: f64) []u8 {
+/// This function writes a custom hexadecimal representation of a
+/// 64-bit floating-point number (`val`) to a provided buffer (`buffer`).
+/// The format uses hexadecimal for the significand and exponent,
+/// with the exponent prefixed by 'p'.
+///
+/// **Input:**
+///
+/// - `buffer (slice)`: Needs at least 21 `u8` elements (not checked).
+/// - `val (f64)`: The 64-bit floating-point number to convert.
+///
+/// **Output:**
+///
+/// (void) - Writes the converted representation directly into the buffer.
+///
+/// **Notes:**
+///
+/// - Non-standard format, not IEEE 754 compatible.
+/// - Assumes sufficient buffer space (>= 21 elements).
+pub fn write(buffer: []u8, val: f64) void {
     inline for (
         .{ 0, 1, 2, 3, 17 },
         .{ '0', 'x', '1', '.', 'p' },
@@ -27,12 +45,37 @@ pub fn write(buffer: []u8, val: f64) []u8 {
         ind -= 1;
     }) buffer[ind] = hexFromInt(tmp & 0xf);
 
-    return buffer[0..];
+    return;
 }
 
 test "write" {
     var buffer: [21]u8 = undefined;
 
-    try std.testing.expect(std.mem.eql(u8, write(&buffer, std.math.pi), "0x1.921fb54442d18p400"));
-    try std.testing.expect(std.mem.eql(u8, write(&buffer, 0x1.fffffffffffffp+1023), "0x1.fffffffffffffp7fe"));
+    // positive numbers
+    write(&buffer, std.math.pi);
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.921fb54442d18p400"));
+    write(&buffer, std.math.nan(f64));
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.8000000000000p7ff"));
+    write(&buffer, std.math.inf(f64));
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.0000000000000p7ff"));
+    write(&buffer, std.math.floatMax(f64));
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.fffffffffffffp7fe"));
+    write(&buffer, std.math.floatMin(f64));
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.0000000000000p001"));
+    write(&buffer, 0x1.5555555555555p-2); // 0.3333333333333333 ≈ 1/3
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.5555555555555p3fd"));
+
+    // negative numbers
+    write(&buffer, -std.math.pi);
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.921fb54442d18pc00"));
+    write(&buffer, -std.math.nan(f64));
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.8000000000000pfff"));
+    write(&buffer, -std.math.inf(f64));
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.0000000000000pfff"));
+    write(&buffer, -std.math.floatMax(f64));
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.fffffffffffffpffe"));
+    write(&buffer, -std.math.floatMin(f64));
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.0000000000000p801"));
+    write(&buffer, -0x1.5555555555555p-2); // 0.3333333333333333 ≈ 1/3
+    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.5555555555555pbfd"));
 }
