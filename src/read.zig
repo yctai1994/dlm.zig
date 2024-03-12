2c9efc9f5c5278d7e78f680c20e2b83bca37d5d7
yctai1994 <yctai.git@gmail.com>
2024-03-12 22:46:55 +0900

    add: `read`; edit: `write`

---
+++ src/read.zig
@@ -0,0 +1,85 @@
+const std = @import("std");
+
+fn intFromHex(hex: u8) u8 {
+    return switch (hex) {
+        '0'...'9' => hex - 48,
+        'a'...'f' => hex - 87,
+        else => unreachable,
+    };
+}
+
+const ReadError = error{InvalidCharacter};
+
+pub fn read(src: []const u8) ReadError!f64 {
+    inline for (
+        .{ 0, 1, 2, 3, 17 },
+        .{ '0', 'x', '1', '.', 'p' },
+    ) |i, c| if (src[i] != c) return ReadError.InvalidCharacter;
+
+    var val: u64 = 0x0;
+    val |= intFromHex(src[18]);
+
+    for (19..21) |ind| {
+        val <<= 4;
+        val |= intFromHex(src[ind]);
+    }
+
+    for (4..17) |ind| {
+        val <<= 4;
+        val |= intFromHex(src[ind]);
+    }
+
+    return @bitCast(val);
+}
+
+test "read" {
+    const val_list = [_]f64{
+        // positive numbers
+        std.math.pi,
+        std.math.nan(f64),
+        std.math.inf(f64),
+        std.math.floatMax(f64),
+        std.math.floatMin(f64),
+        0x1.5555555555555p-2, // 0.3333333333333333 ≈ 1/3
+        // negative numbers
+        -std.math.pi,
+        -std.math.nan(f64),
+        -std.math.inf(f64),
+        -std.math.floatMax(f64),
+        -std.math.floatMin(f64),
+        -0x1.5555555555555p-2, // 0.3333333333333333 ≈ 1/3
+    };
+
+    const str_list = [_][]const u8{
+        // positive numbers
+        "0x1.921fb54442d18p400",
+        "0x1.8000000000000p7ff",
+        "0x1.0000000000000p7ff",
+        "0x1.fffffffffffffp7fe",
+        "0x1.0000000000000p001",
+        "0x1.5555555555555p3fd",
+        // negative numbers
+        "0x1.921fb54442d18pc00",
+        "0x1.8000000000000pfff",
+        "0x1.0000000000000pfff",
+        "0x1.fffffffffffffpffe",
+        "0x1.0000000000000p801",
+        "0x1.5555555555555pbfd",
+    };
+
+    for (val_list, str_list) |val, str| {
+        if (read(str)) |tmp| {
+            std.testing.expectEqual(
+                @as(u64, @bitCast(tmp)),
+                @as(u64, @bitCast(val)),
+            ) catch |err| {
+                std.debug.print(
+                    "{any} at {x} vs. {s}, got {x}\n",
+                    .{ err, val, str, tmp },
+                );
+            };
+        } else |err| {
+            if (err == ReadError.InvalidCharacter) continue else unreachable;
+        }
+    }
+}
--- src/write.zig
+++ src/write.zig
@@ -9,13 +9,13 @@ fn hexFromInt(int: u64) u8 {
 }
 
 /// This function writes a custom hexadecimal representation of a
-/// 64-bit floating-point number (`val`) to a provided buffer (`buffer`).
+/// 64-bit floating-point number (`val`) to a provided buffer (`des`).
 /// The format uses hexadecimal for the significand and exponent,
 /// with the exponent prefixed by 'p'.
 ///
 /// **Input:**
 ///
-/// - `buffer (slice)`: Needs at least 21 `u8` elements (not checked).
+/// - `des (slice)`: Needs at least 21 `u8` elements (not checked).
 /// - `val (f64)`: The 64-bit floating-point number to convert.
 ///
 /// **Output:**
@@ -26,56 +26,73 @@ fn hexFromInt(int: u64) u8 {
 ///
 /// - Non-standard format, not IEEE 754 compatible.
 /// - Assumes sufficient buffer space (>= 21 elements).
-pub fn write(buffer: []u8, val: f64) void {
+pub fn write(des: []u8, val: f64) void {
     inline for (
         .{ 0, 1, 2, 3, 17 },
         .{ '0', 'x', '1', '.', 'p' },
-    ) |i, c| buffer[i] = c;
+    ) |i, c| des[i] = c;
 
     var tmp: u64 = @bitCast(val);
     var ind: u8 = 16;
     while (3 < ind) : ({
         tmp >>= 4;
         ind -= 1;
-    }) buffer[ind] = hexFromInt(tmp & 0xf);
+    }) des[ind] = hexFromInt(tmp & 0xf);
 
     ind = 20;
-    while (17 < ind) : ({
+    while (18 < ind) : ({
         tmp >>= 4;
         ind -= 1;
-    }) buffer[ind] = hexFromInt(tmp & 0xf);
+    }) des[ind] = hexFromInt(tmp & 0xf);
+    des[18] = hexFromInt(tmp & 0xf);
 
     return;
 }
 
 test "write" {
+    const val_list = [_]f64{
+        // positive numbers
+        std.math.pi,
+        std.math.nan(f64),
+        std.math.inf(f64),
+        std.math.floatMax(f64),
+        std.math.floatMin(f64),
+        0x1.5555555555555p-2, // 0.3333333333333333 ≈ 1/3
+        // negative numbers
+        -std.math.pi,
+        -std.math.nan(f64),
+        -std.math.inf(f64),
+        -std.math.floatMax(f64),
+        -std.math.floatMin(f64),
+        -0x1.5555555555555p-2, // 0.3333333333333333 ≈ 1/3
+    };
+
+    const str_list = [_][]const u8{
+        // positive numbers
+        "0x1.921fb54442d18p400",
+        "0x1.8000000000000p7ff",
+        "0x1.0000000000000p7ff",
+        "0x1.fffffffffffffp7fe",
+        "0x1.0000000000000p001",
+        "0x1.5555555555555p3fd",
+        // negative numbers
+        "0x1.921fb54442d18pc00",
+        "0x1.8000000000000pfff",
+        "0x1.0000000000000pfff",
+        "0x1.fffffffffffffpffe",
+        "0x1.0000000000000p801",
+        "0x1.5555555555555pbfd",
+    };
+
     var buffer: [21]u8 = undefined;
 
-    // positive numbers
-    write(&buffer, std.math.pi);
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.921fb54442d18p400"));
-    write(&buffer, std.math.nan(f64));
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.8000000000000p7ff"));
-    write(&buffer, std.math.inf(f64));
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.0000000000000p7ff"));
-    write(&buffer, std.math.floatMax(f64));
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.fffffffffffffp7fe"));
-    write(&buffer, std.math.floatMin(f64));
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.0000000000000p001"));
-    write(&buffer, 0x1.5555555555555p-2); // 0.3333333333333333 ≈ 1/3
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.5555555555555p3fd"));
-
-    // negative numbers
-    write(&buffer, -std.math.pi);
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.921fb54442d18pc00"));
-    write(&buffer, -std.math.nan(f64));
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.8000000000000pfff"));
-    write(&buffer, -std.math.inf(f64));
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.0000000000000pfff"));
-    write(&buffer, -std.math.floatMax(f64));
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.fffffffffffffpffe"));
-    write(&buffer, -std.math.floatMin(f64));
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.0000000000000p801"));
-    write(&buffer, -0x1.5555555555555p-2); // 0.3333333333333333 ≈ 1/3
-    try std.testing.expect(std.mem.eql(u8, &buffer, "0x1.5555555555555pbfd"));
+    for (val_list, str_list) |val, str| {
+        write(&buffer, val);
+        std.testing.expect(std.mem.eql(u8, &buffer, str)) catch |err| {
+            std.debug.print(
+                "{any} at {x} vs. {s}, got {s}\n",
+                .{ err, val, str, buffer },
+            );
+        };
+    }
 }
